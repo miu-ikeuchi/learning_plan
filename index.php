f9d718d10de36d200ef8921a5824b9d1a828b438
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-04-26 20:54:25 +0900

    見た目調整

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+require_once __DIR__ . '/functions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<?php include_once __DIR__ . '/_head.html' ?>
+
+<body>
+    <div class="wrapper">
+        <h1 class="title">学習管理アプリ</h1>
+        <div class="form-area">
+            <!-- エラー表示 -->
+
+            <form action="" method="post">
+                <label for="title">学習内容</label>
+                <input type="text" name="title">
+                <label for="due_date">期限日</label>
+                <input type="date" name="due_date">
+                <input type="submit" class="btn submit-btn" value="追加">
+            </form>
+        </div>
+        <div class="incomplete-area">
+            <h2 class="sub-title">未達成</h2>
+            <table class="plan-list">
+                <thead>
+                    <tr>
+                        <th class="plan-title">学習内容</th>
+                        <th class="plan-due-date">完了期限</th>
+                        <th class="done-link-area"></th>
+                        <th class="edit-link-area"></th>
+                        <th class="delete-link-area"></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <!-- 未完了のデータを表示 -->
+
+                </tbody>
+            </table>
+        </div>
+        <div class="complete-area">
+            <h2 class="sub-title">完了</h2>
+            <table class="plan-list">
+                <thead>
+                    <tr>
+                        <th class="plan-title">学習内容</th>
+                        <th class="plan-completion-date">完了日</th>
+                        <th class="done-link-area"></th>
+                        <th class="edit-link-area"></th>
+                        <th class="delete-link-area"></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <!-- 完了済のデータを表示 -->
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
